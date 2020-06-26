2d52f7f9b66c22065f5f3673d6232e0d3f7ef048
petejb88 <petejb88@users.noreply.github.com>
2020-06-26 17:27:16 -0400

    added notearray cleaning script, part 1

---
+++ tools/notearray_to_df.py
@@ -0,0 +1,63 @@
+# ---------- Convert notearray to dataframe ----------
+# Author: Peter Bonventre
+# Goal: Take in a josquin notearray (or more generally any notearray from a humdrum file) and product a dataframe with the same data
+
+import pandas as pd
+from io import StringIO
+
+import argparse
+
+parser = argparse.ArgumentParser(description="Convert a notearray into a clean df")
+parser.add_argument('notearray_path', help="Path to notearray")
+
+args = parser.parse_args()
+
+
+
+
+
+
+
+def clean_notearray(notearray_path):
+    '''
+    args:
+         notearray_path: location of notearray.txt
+    output:
+         lines: a list of lines to be read by pandas as a csv
+    '''
+    
+    with open(notearray_path, "r") as notearray:
+        lines = notearray.readlines()
+        
+        # find start line
+        header_lines = [ii for ii,line in enumerate(lines) if line.count('!') != 0]
+        start_line_num = min(set(range(len(lines)))-set(header_lines))
+        start_line = lines[start_line_num]
+        end_line_num = max(set(range(len(lines))) - set(header_lines))
+
+        # clean file
+        lines = lines[start_line_num : end_line_num+1]
+    
+        # clean start_line
+        start_line = start_line.replace('%','')
+        lines[0] = start_line
+        del lines[1]
+
+        # act as newfile
+        cleaned_notearray = StringIO('\n'.join(lines))
+
+        return cleaned_notearray
+        
+
+
+
+
+
+def main():
+    cleaned_notearray = clean_notearray(args.notearray_path)
+    df = pd.read_csv(cleaned_notearray, delimiter='\t')
+    # find: number of voices, ... actually, this was significantly less work than I thought
+    print(df.columns)
+
+if __name__ == "__main__":
+    main()
